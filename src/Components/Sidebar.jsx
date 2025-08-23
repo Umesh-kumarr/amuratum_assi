bd1567a5a119ec65a10ee508898181931fd1f8a6
UMESH KUMAR <246810umesh@gmail.com>
2025-08-23 09:51:38 +0530

    change to capital letters in the route paths in App.jsx

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import dashboard from '../assets/sidebar/dashboard.svg';
 import patients from '../assets/sidebar/patients.svg';
-import doctorschedule from '../assets/sidebar/Doctorschedule.svg';
+import doctorschedule from '../assets/sidebar/doctorschedule.svg';
 import appointments from '../assets/sidebar/appointments.svg';
 import chat from '../assets/sidebar/chat.svg';
 import consultation from '../assets/sidebar/consultation.svg';
